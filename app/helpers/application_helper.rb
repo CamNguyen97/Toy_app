0418c3c42ee7aef7bec9e80dbe693cafb8993425
camnh <hongcam941997@gmail.com>
2018-06-06 08:54:42 +0700

    Chap7_sign_up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
    def full_title(page_title = "")
     page_title.nil? ? t("title") : page_title + " | " + t("title")
   end
+
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
