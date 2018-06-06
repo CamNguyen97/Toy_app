9c2c81773f1487f1678bca84c1f187664fe62673
camnh <hongcam941997@gmail.com>
2018-06-06 09:40:43 +0700

    fix_comment_first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_create { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
   validates :email, presence: true, length: { maximum: 255 },
